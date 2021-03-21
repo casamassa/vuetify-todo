8a1a9ecc917c78577f47f82b03e080a192ae6d44
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-03-21 16:12:59 -0300

    todo component implementation

--- src/App.vue
+++ src/App.vue
@@ -37,10 +37,37 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="mountains.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -54,7 +81,7 @@
     data: () => ({ 
       drawer: null,
       items: [
-          { title: 'Todo', icon: 'mdi-view-dashboard', to: '/' },
+          { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
           { title: 'About', icon: 'mdi-help-box', to: '/about' },
         ]
     }),
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, assumenda voluptatum. Ipsa adipisci quia fugit harum, nulla nesciunt nihil ea necessitatibus nam saepe est quisquam temporibus quam ut? Totam, possimus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, assumenda voluptatum. Ipsa adipisci quia fugit harum, nulla nesciunt nihil ea necessitatibus nam saepe est quisquam temporibus quam ut? Totam, possimus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, assumenda voluptatum. Ipsa adipisci quia fugit harum, nulla nesciunt nihil ea necessitatibus nam saepe est quisquam temporibus quam ut? Totam, possimus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, assumenda voluptatum. Ipsa adipisci quia fugit harum, nulla nesciunt nihil ea necessitatibus nam saepe est quisquam temporibus quam ut? Totam, possimus?</p>
   </div>
 </template>
